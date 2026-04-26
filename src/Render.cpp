87105709e457fdcc33402d9f4c5e0ef1c7a68b95
Bill Rimell <billjrimell@gmail.com>
2026-04-26 02:40:48 +0100

    Use optional depth fetch helper in render path

--- src/Render.cpp
+++ src/Render.cpp
@@ -758,6 +758,10 @@ bool fetchImage(OfxImageClipHandle clip, OfxTime time, OfxPropertySetHandle *ima
 
 bool fetchOptionalImage(OfxImageClipHandle clip, OfxTime time, OfxPropertySetHandle *imageHandle,
                         Image *image) {
+  if (!imageHandle || !image) {
+    return false;
+  }
+
   *imageHandle = nullptr;
   *image = {};
 
@@ -954,8 +958,8 @@ OfxStatus render(OfxImageEffectHandle instance, OfxPropertySetHandle inArgs) {
             const bool depthClipAvailable = depthClip != nullptr;
             const bool depthClipIsConnected = clipConnected(depthClip);
             const bool depthImageFetched =
-              depthRequested && depthClipAvailable && depthClipIsConnected &&
-              fetchImage(depthClip, time, &depthImageHandle, &depth);
+              depthRequested && depthClipAvailable &&
+              fetchOptionalImage(depthClip, time, &depthImageHandle, &depth);
             bool hasDepth = depthImageFetched;
             logPrintf(LogLevel::Debug,
                   "render",
