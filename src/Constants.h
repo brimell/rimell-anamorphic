b96ecc1b9b6c4eb3a19cb852fcf02983ff8a3fd7
Bill Rimell <billjrimell@gmail.com>
2026-04-26 03:51:56 +0100

    Bump plugin version for Resolve reload

--- src/Constants.h
+++ src/Constants.h
@@ -4,7 +4,7 @@ namespace rimell {
 
 inline constexpr const char *kPluginIdentifier = "com.rimell.ofx.anamorphic";
 inline constexpr int kPluginMajorVersion = 0;
-inline constexpr int kPluginMinorVersion = 1;
+inline constexpr int kPluginMinorVersion = 2;
 inline constexpr float kPi = 3.14159265358979323846f;
 
 } // namespace rimell
